9fbf97cd5b22c61c83c61a817d5090442ac15ed4
Iliasu Salifu <iliasu.salifu@seamless.se>
2024-07-29 17:32:52 +0000

    commit to complete character creator assignment

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import './index.css';
+import Backdrop from './components/Backdrop';
 
 function App() {
   return (
     <>
+      <Backdrop />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/Backdrop/Backdrop.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Backdrop.module.css';
+
+const Backdrop = () => {
+  return <div className={styles.decoration}></div>;
+};
+
+export default Backdrop;
